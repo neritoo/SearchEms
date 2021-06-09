9a54c321ded7ee5af12cb02c4afc696b7c5e97d6
neritoo <ezegavilan95@gmail.com>
2021-06-09 18:00:41 -0300

    Comentarios en la API buscador.

--- searchems-searcher/src/main/java/com/gavilan/searchems/buscador/controllers/BuscadorController.java
+++ searchems-searcher/src/main/java/com/gavilan/searchems/buscador/controllers/BuscadorController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author Eze Gavilan
  * @project SearchEms
@@ -19,11 +23,19 @@ public class BuscadorController {
     @GetMapping("/buscador")
     public ResponseEntity<?> buscarConsulta(@RequestParam(name = "consulta") String consulta) {
         System.out.println("Consulta: " + consulta);
-        String[] terminos = consulta.split(" ");
-        for (String t: terminos) {
-            System.out.println(t);
-        }
 
+        // Obviamente esto no lo hace la API, si no que el servicio que debemos implementar (probablemente "BuscadorService").
+        // Es más, debería hacerlo otro service llamado, por ej.: "SeparadorPalabras", pero podría hacerse en un método
+        // privado del BuscadorService...
+        List<String> terminos = Arrays.stream(consulta.split(" "))
+                .map(String::toLowerCase).collect(Collectors.toList());
+        terminos.forEach(System.out::println);
+
+        // La API, entonces, deberá DELEGAR la consulta a un servicio, que nos devolverá un LISTADO DOCUMENTOS más
+        // relevantes de la consulta, que es lo que "mostraremos" (devolveremos)...
+
+        // Entonces, la API depende del caso de uso buscar documentos relevantes, que tiene la lógica para, a través de
+        // un rankeo, calcular y devolver los documentos con mayor puntos de ranking para la consulta solicitada...
         return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 
