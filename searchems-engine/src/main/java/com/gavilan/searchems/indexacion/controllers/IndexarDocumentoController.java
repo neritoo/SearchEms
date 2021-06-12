b44061a328111b33446f73b8545574f02ebc3b53
neritoo <ezegavilan95@gmail.com>
2021-06-12 13:33:05 -0300

    diseñada API para agregar e indexar nuevo documento.

--- searchems-engine/src/main/java/com/gavilan/searchems/indexacion/controllers/IndexarDocumentoController.java
+++ searchems-engine/src/main/java/com/gavilan/searchems/indexacion/controllers/IndexarDocumentoController.java
@@ -1,10 +1,22 @@
 package com.gavilan.searchems.indexacion.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
 public class IndexarDocumentoController {
+
+    /**
+     * API para agregar un nuevo documento a la base de datos, e indexarlo al vocabulario y lista de posteo.
+     * @param documento {@link MultipartFile} con el archivo del documento propiamente dicho.
+     * @return ResponseEntity.
+     */
+    public ResponseEntity<?> indexarDocumento(@RequestParam(name = "documento")MultipartFile documento) {
+        return null;
+    }
 
 }
